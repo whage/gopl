6181f394b333b560a479a1456cc6c3257122ed5d
András Sallai <andras.sallai@evosoft.com>
2020-02-13 14:57:39 +0100

    Use custom types for points and polygons

--- surface.go
+++ surface.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"math"
+	//"os"
 )
 
 const (
 	width, height = 600, 320 // canvas size in pixels
-	cells = 50 // number of grid cells
+	cells = 15 // number of grid cells
 	xyrange = 30.0 // axis ranges (-xyrange..+xyrange)
 	xyscale = width / 2 / xyrange // pixels per x or y unit
 	zscale = height * 0.3 // pixels per z unit
@@ -16,40 +17,73 @@ const (
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
-type Point struct {
+type Point3D struct {
 	X, Y, Z float64
 }
 
-type Quad struct {
-	A, B, C, D Point
+type Point2D struct {
+	X, Y float64
 }
 
-func (q *Quad) GetAverageHeight() float64 {
+type Quad3D struct {
+	A, B, C, D Point3D
+}
+
+type Quad2D struct {
+	A, B, C, D Point2D
+}
+
+func (q *Quad3D) GetAverageHeight() float64 {
 	return (q.A.Z + q.B.Z + q.C.Z + q.D.Z) / 4
+}
+
+func project3DPoint(x, y, z float64) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	//x := xyrange * (float64(i)/cells)
+	//y := xyrange * (float64(j)/cells)
+
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+
+	//fmt.Fprintln(os.Stderr, sy)
+
+	if math.IsNaN(sy) {
+		//sy = 0
+	}
+
+	return sx, sy
+}
+
+func (q *Quad3D) ProjectTo2D() Quad2D {
+	ax, ay := project3DPoint(q.A.X, q.A.Y, q.A.Z)
+	bx, by := project3DPoint(q.B.X, q.B.Y, q.B.Z)
+	cx, cy := project3DPoint(q.C.X, q.C.Y, q.C.Z)
+	dx, dy := project3DPoint(q.D.X, q.D.Y, q.D.Z)
+
+	return Quad2D{
+		Point2D{ax, ay},
+		Point2D{bx, by},
+		Point2D{cx, cy},
+		Point2D{dx, dy},
+	}
 }
 
 func main() {
 	var minZ float64 = 0
 	var maxZ float64 = 0
 
-	polygons := make([]Quad, 0)
+	polygons := make([]Quad3D, 0)
 
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			ax, ay, az := corner(i+1, j)
-			bx, by, bz := corner(i, j)
-			cx, cy, cz := corner(i, j+1)
-			dx, dy, dz := corner(i+1, j+1)
-
-			q := Quad{
-				Point{ax, ay, az},
-				Point{bx, by, bz},
-				Point{cx, cy, cz},
-				Point{dx, dy, dz},
+	for i := -15.0; i < cells; i++ {
+		for j := -15.0; j < cells; j++ {
+			q := Quad3D{
+				Point3D{i+1, j, f(i+1, j)},
+				Point3D{i, j, f(i, j)},
+				Point3D{i, j+1, f(i, j+1)},
+				Point3D{i+1, j+1, f(i+1, j+1)},
 			}
-
+			
 			polygons = append(polygons, q)
-
 			averageZ := q.GetAverageHeight()
 
 			if averageZ < minZ {
@@ -61,13 +95,16 @@ func main() {
 			}
 		}
 	}
+
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width:0.7' "+
 		"width='%d' height='%d'>", width, height)
 
-	for _, p := range polygons {
-		redComponent := getRedComponentAt(p.GetAverageHeight(), minZ, maxZ) 
-		blueComponent := getBlueComponentAt(p.GetAverageHeight(), minZ, maxZ)
+	for _, q := range polygons {	
+		redComponent := getRedComponentAt(q.GetAverageHeight(), minZ, maxZ) 
+		blueComponent := getBlueComponentAt(q.GetAverageHeight(), minZ, maxZ)	
+		p := q.ProjectTo2D()
+
 		fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' style='fill: #%02x00%02x';/>\n", p.A.X, p.A.Y, p.B.X, p.B.Y, p.C.X, p.C.Y, p.D.X, p.D.Y, redComponent, blueComponent)
 	}
 
@@ -83,23 +120,7 @@ func getBlueComponentAt(z, minZ, maxZ float64) int {
 	return int(255 - (z - minZ) * 255/(maxZ - minZ))
 }
 
-func corner(i, j int) (float64, float64, float64) {
-	// Find point (x,y) at corner of cell (i,j).
-	x := xyrange * (float64(i)/cells - 0.5)
-	y := xyrange * (float64(j)/cells - 0.5)
-
-	// Compute surface height z.
-	z := f(x, y)
-
-	// Project (x,y,z) isometrically onti 2-D SVG canvas (sx, sy).
-	sx := width/2 + (x-y)*cos30*xyscale
-	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy, z
-}
-
 func f(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
 	return math.Sin(r) / r
-	//return math.Pow(x, 2)/100 - math.Pow(y, 2)/100
-	//return math.Pow(x, 3)/100 - 3*x*math.Pow(y, 2)/100
 }
